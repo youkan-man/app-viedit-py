fc833173c3fe0d87fc65f7a992514c539513f4fb
youkan-man <okabe.tatsuya@nichiwa-system.co.jp>
2026-09-11 21:27:25 +0900

    fix: normalize terminal coordinate spaces before projected fit

--- app/static/vi-editor-runtime-fixes.js
+++ app/static/vi-editor-runtime-fixes.js
@@ -17,6 +17,7 @@
     ['vi-editor-component-primer', '/static/vi-editor-component-primer.js?v=1'],
     ['vi-editor-component-projection', '/static/vi-editor-component-projection.js?v=1'],
     ['vi-editor-component-anchor', '/static/vi-editor-component-anchor.js?v=1'],
+    ['vi-editor-component-coordinate-space', '/static/vi-editor-component-coordinate-space.js?v=1'],
     ['vi-editor-component-fit', '/static/vi-editor-component-fit.js?v=1'],
   ];
 
